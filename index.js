04b03531b1f8fe6a7587d2a1959e6886c475c894
gsoumya <gsoumya515@gmail.com>
2019-06-10 16:24:27 +0530

    added dishes schema with comments(nested schema)

--- index.js
+++ index.js
@@ -7,14 +7,29 @@ const connect = mongoose.connect(url)
 connect.then((db)=> {
     console.log('connceted correctly to server');
     dishes.create({
-        name: 'Pani puri',
+        name: 'dahi wada',
         description: 'very tasty'
     }).then((dish)=> {
         console.log(dish, 'dish');
-      return  dishes.find({}).exec();
-    }).then((dishes)=> {
-        console.log(dishes, 'all dishes');
-       return mongoose.connection.close()
+        return dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
+    }).then((dish)=> {
+        console.log(dish, 'dish after update');
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+      // return mongoose.connection.close()
+    }).then((dish)=> {
+        console.log(dish, 'dish with comment')
+        return mongoose.connection.close()
     }).catch((error)=> {
         console.log(error, 'error')
     })
--- models/dishes.js
+++ models/dishes.js
@@ -1,19 +1,40 @@
 const mongoose = require('mongoose');
-const Schema =mongoose.Schema;
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+        timestamps: true
+    })
 
 const dishSchema = new Schema({
-    name : {
+    name: {
         required: true,
         type: String,
         unique: true
     },
-    description : {
+    description: {
         required: true,
         type: String
-    }},
+    },
+    comments: [commentSchema]
+},
     {
         timestamps: true
     })
 
-    var dishes = mongoose.model("dish", dishSchema);
-    module.exports = dishes;
+var dishes = mongoose.model("dish", dishSchema);
+module.exports = dishes;
